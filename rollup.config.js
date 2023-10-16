5a7e578ab5118e6f99845479dd2f6f741b87876a
chuongmep <chuongpqvn@gmail.com>
2023-10-16 20:47:26 +0800

    fix github page

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import css from'rollup-plugin-css-only'
+import html from 'rollup-plugin-html';
+import copy from 'rollup-plugin-copy';
 export default {
   input: './src/index.js',
   output: [
@@ -12,5 +14,21 @@ export default {
   plugins: [
     resolve(),
     css({output:'bundle.css'}),
+    html({
+      include: '**/*.html',
+      htmlMinifierOptions: {
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        conservativeCollapse: true,
+        minifyCSS: true,
+        minifyJS: true,
+      }
+    }),
+    copy({
+      targets: [
+        { src: 'src/*.html', dest: 'dist' }, // Copy all HTML files from src to dist
+        { src: './wasm', dest: 'dist' }, // Copy the entire 'wasm' folder
+      ],
+    }),
   ]
 };
